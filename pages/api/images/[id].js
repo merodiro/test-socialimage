f8ba9952bccc61b5f4bcf3bff4b7af54947c3496
Amr Gad <merodiro@gmail.com>
2021-02-08 23:40:04 +0200

    use firebase data

--- pages/api/images/[id].js
+++ pages/api/images/[id].js
@@ -3,12 +3,26 @@ import chrome from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-core'
 import faker from 'faker'
 
+import firestore from '../../../utils/firestore'
+
 export default async (req, res) => {
+  const snapshot = await firestore.collection('raid-stats').doc(req.query.id).get()
+  if (!snapshot.exists) {
+    return res.status(404).send({
+      message: 'not found',
+    })
+  }
+
+  const document = snapshot.data()
+
   const image = await renderSocialImage({
     template: 'basic',
     templateParams: {
-      imageUrl: 'https://images.unsplash.com/photo-1557958114-3d2440207108?w=1950&q=80',
-      title: faker.lorem.sentence(),
+      // imageUrl: 'https://images.unsplash.com/photo-1557958114-3d2440207108?w=1950&q=80',
+      unsplashKeywords: 'raid',
+      title: document.title,
+      logo: 'https://avatars.githubusercontent.com/u/75751120?s=200&v=4',
+      watermark: document.dungeon,
     },
     size: 'facebook',
     ...(process.env.NODE_ENV === 'production'
--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <img src="/api/images/1" />
+  return <img src="/api/images/pjmIlzA4UadHh8m2XIga" />
 }
